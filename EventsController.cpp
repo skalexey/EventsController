e8d9cedfdcfcb1e8a6a61eec2ab9c8ebc5860f7a
skalexey <skorohodov-ad@narod.ru>
2017-12-09 15:22:14 +0300

    Initial commit

---
+++ EventsController.cpp
@@ -0,0 +1,79 @@
+//
+//  EventsController.cpp
+//  Cloud Raiders Debug
+//
+//  Created by skorokhodov on 11/9/17.
+//
+#include <stdio.h>
+#include "EventsController.h"
+
+bool EventsController::mVerbose(true);
+
+EventsController& EventsController::instance()
+{
+    static EventsController _instance;
+    return _instance;
+}
+
+EventsController::EventsController()
+{
+    
+}
+
+void EventsController::Log(const char* fmt, ...)
+{
+    char buf[512];
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buf, 512, fmt, args);
+    buf[511] = '\0';
+    va_end(args);
+    printf("EventsController: %s\n", buf);
+}
+
+void EventsController::unsubscribeFromEvent(Event event, void* subscriber)
+{
+    Log("%ld unsubscribe from event %u", subscriber, (unsigned)event);
+    Subscriptions::iterator it = mSubscriptions.find(subscriber);
+    if(it == mSubscriptions.end())
+    {
+        Log("Warning! Not existing subscriber %ld try to unsubscribe from event %u", subscriber, (unsigned)event);
+        return;
+    }
+    mSubscriptions.erase(it);
+}
+
+void EventsController::subscribeToEvent(Event event, const EventCb& callback, void* subscriber)
+{
+    Log("%ld subscribe to event %u", subscriber, (unsigned)event);
+    Subscriptions::iterator it = mSubscriptions.find(subscriber);
+    if(it == mSubscriptions.end())
+    {
+        mSubscriptions[subscriber];
+    }
+    SubscriberSubscriptions& subscriberSubscriptions = mSubscriptions[subscriber];
+    subscriberSubscriptions.insert(Subscription(event, callback));
+}
+void EventsController::fireEvent(Event event, void* data) const
+{
+    Log("Fire event %u", (unsigned)event);
+    bool haveSubscriptions = false;
+    for(auto&& subscriberSubscriptionsEntry : mSubscriptions)
+    {
+        const SubscriberSubscriptions& subscriberSubscriptions = subscriberSubscriptionsEntry.second;
+        auto&& range = subscriberSubscriptions.equal_range(event);
+        if(range.first == subscriberSubscriptions.end())
+        {
+            continue;
+        }
+        for(auto it = range.first; it != range.second; ++it)
+        {
+            haveSubscriptions = true;
+            it->second(data);
+        }
+    }
+    if(!haveSubscriptions)
+    {
+        Log("No subscriptions to event %u", (unsigned)event);
+    }
+}
---
+++ EventsController.h
@@ -0,0 +1,39 @@
+//
+//  EventsController.h
+//  Cloud Raiders Debug
+//
+//  Created by skorokhodov on 11/9/17.
+//
+
+#ifndef EventsController_h
+#define EventsController_h
+
+#include <string>
+#include <unordered_map>
+
+enum Event : unsigned char
+{
+    EVENT_A,
+    EVENT_B
+};
+
+class EventsController
+{
+private:
+    EventsController();
+public:
+    typedef std::function<void(void*)> EventCb;
+    void subscribeToEvent(Event event, const EventCb& cb, void* subscriber);
+    void unsubscribeFromEvent(Event event, void* subscripber);
+    void fireEvent(Event event_name, void* data = nullptr) const;
+    static EventsController& instance();
+private:
+    static void Log(const char* fmt, ...);
+    typedef std::pair<Event, EventCb> Subscription;
+    typedef std::unordered_multimap<Event, EventCb> SubscriberSubscriptions;
+    typedef std::unordered_map<void*, SubscriberSubscriptions> Subscriptions;
+    Subscriptions mSubscriptions;
+    static bool mVerbose;
+};
+
+#endif /* EventsController */
